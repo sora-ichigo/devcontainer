03fe18993417e6778c2121bbca55d5cfeb8e8df0
igsr5 <igsr5.dev@gmail.com>
2023-08-06 17:55:34 +0900

    Update ruby version to 1.0.10 and add apt-get update

--- features/ruby/install.sh
+++ features/ruby/install.sh
@@ -9,6 +9,14 @@ set -e
 # Clean up
 rm -rf /var/lib/apt/lists/*
 
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
 # Checks if packages are installed and installs them if not
 check_packages() {
     if ! dpkg -s "$@" > /dev/null 2>&1; then
@@ -16,6 +24,7 @@ check_packages() {
         apt-get -y install --no-install-recommends "$@"
     fi
 }
+
 
 # Ensure apt is in non-interactive to avoid prompts
 export DEBIAN_FRONTEND=noninteractive
