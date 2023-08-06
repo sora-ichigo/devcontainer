05be585691597b91f4187c515aa933b023acc80a
igsr5 <igsr5.dev@gmail.com>
2023-08-06 16:33:31 +0900

    Add PostgreSQL installation to install script

--- features/common-tools/install.sh
+++ features/common-tools/install.sh
@@ -4,6 +4,7 @@ set -e
 
 USER_NAME="vscode"
 
+# postgresqlのインストール
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list \
 && wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - \
 && apt-get update \
