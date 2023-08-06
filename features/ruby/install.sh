10a878c7306c978fd6e6b7a210608bb44a9ced69
igsr5 <igsr5.dev@gmail.com>
2023-08-06 17:07:37 +0900

    Update Ruby version to 1.0.4 and add installation script

--- features/ruby/install.sh
+++ features/ruby/install.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-USER_NAME=vscode
+USER_NAME="vscode"
+
 su $USER_NAME <<EOF
 git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
 echo 'eval "$(~/.rbenv/bin/rbenv init - bash)"' >> $HOME/.bashrc
 git clone https://github.com/rbenv/ruby-build.git "$($HOME/.rbenv/bin/rbenv root)"/plugins/ruby-build
 $HOME/.rbenv/bin/rbenv install 3.1.4
 $HOME/.rbenv/bin/rbenv global 3.1.4
-
-echo "done!"
 EOF
 
-
-echo "done"
+echo "done!"
