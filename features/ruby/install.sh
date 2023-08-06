c2fccf3f3231438dd79f061ec0a0889abec1031a
igsr5 <igsr5.dev@gmail.com>
2023-08-06 18:40:10 +0900

    Update ruby devcontainer version and rbenv init pathThe maximum length of the commit message is 50 characters. The original commit message exceeds this limit

--- features/ruby/install.sh
+++ features/ruby/install.sh
@@ -61,7 +61,7 @@ if [[ ! -d "/usr/local/share/ruby-build" ]]; then
 fi
 
 echo 'eval "$(${HOME}/.rbenv/bin/rbenv init - bash)"' >> /home/${USERNAME}/.bashrc
-eval "$(${HOME}/.rbenv/bin/rbenv init - bash)"
+eval "$(/home/${USERNAME}/.rbenv/bin/rbenv init - bash)"
 rbenv install ${RUBY_VERSION}
 
 # Clean up
