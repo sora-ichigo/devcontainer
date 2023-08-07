b68261666f899e61ffc17cf6d501cbb37779fefa
igsr5 <igsr5.dev@gmail.com>
2023-08-08 00:40:49 +0900

    Update common-tools version to 1.0.9, add username variable

--- features/common-tools/install.sh
+++ features/common-tools/install.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
+RUBY_VERSION=${VERSION}
+
 set -e
 
-USER_NAME="vscode"
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+# Ensure apt is in non-interactive to avoid prompts
+export DEBIAN_FRONTEND=noninteractive
 
 # postgresqlのインストール
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list \
@@ -27,7 +50,7 @@ apt-get update && export DEBIAN_FRONTEND=noninteractive \
     tmux neovim fzf ripgrep
 
 # ユーザーを変更してコマンドを実行
-su $USER_NAME <<EOF
+su $USERNAME <<EOF
 mkdir -p \$HOME/bash
 cp bash/alias.bash \$HOME/bash/alias.bash
 echo 'source \$HOME/bash/alias.bash' >> \$HOME/.bashrc
