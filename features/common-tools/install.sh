c59fad6b7bbff3a15bb70e8f8ebfd0e7c04cd427
igsr5 <igsr5.dev@gmail.com>
2023-08-07 20:39:21 +0900

    Update common-tools version to 1.0.2 and make minor changes to install script

--- features/common-tools/install.sh
+++ features/common-tools/install.sh
@@ -1,42 +1,53 @@
 #!/bin/bash
 
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
+
 set -e
 
-USER_NAME="vscode"
+# Clean up
+rm -rf /var/lib/apt/lists/*
 
-# postgresqlのインストール
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+create_psql_user() {
+    service postgresql start
+    su postgres -c "psql --command \"CREATE ROLE vscode LOGIN SUPERUSER\""
+    sed -i 's/md5/trust/g'  /etc/postgresql/13/main/pg_hba.conf
+    sed -i 's/peer/trust/g'  /etc/postgresql/13/main/pg_hba.conf
+}
+
+# Install postgresql-13
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list \
-&& wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - \
-&& apt-get update \
-&& apt-get -y install postgresql-13 \
-&& service postgresql start \
-&& su postgres -c "psql --command \"CREATE ROLE vscode LOGIN SUPERUSER\"" \
-&& sed -i 's/md5/trust/g'  /etc/postgresql/13/main/pg_hba.conf \
-&& sed -i 's/peer/trust/g'  /etc/postgresql/13/main/pg_hba.conf
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+check_packages postgresql-13
+create_psql_user
 
-# パッケージのインストール (依存)
-apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && apt-get -y install --no-install-recommends \
-    libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev \
+# Install dependencies (dependencies for development)
+check_packages libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev \
     libnss3-dev iptables libx11-dev libxtst6 sshfs libncurses5-dev libncursesw5-dev libpq-dev \
     git-core bash-completion
 
-# パッケージのインストール (ツール)
-apt-get update && export DEBIAN_FRONTEND=noninteractive \
-&& apt-get -y install --no-install-recommends \
-    tmux neovim fzf ripgrep
+# Install dependencies (development tools)
+check_packages tmux neovim fzf ripgrep
 
-# ユーザーを変更してコマンドを実行
-su $USER_NAME <<EOF
-mkdir -p \$HOME/bash
-cp bash/alias.bash \$HOME/bash/alias.bash
-echo 'source \$HOME/bash/alias.bash' >> \$HOME/.bashrc
-cp bash/functions.bash \$HOME/bash/functions.bash
-echo 'source \$HOME/bash/functions.bash' >> \$HOME/.bashrc
-cp bash/.bash_history \$HOME/.bash_history
-cp bash/prompt.bash \$HOME/bash/prompt.bash
-echo 'source \$HOME/bash/prompt.bash' >> \$HOME/.bashrc
-EOF
+mkdir /home${USERNAME}/bash
+cp -r bash /home/${USERNAME}/bash
+cp .bash_history_template /home/${USERNAME}/.bash_history
+echo 'souce $HOME/bash/*' >> /home/${USERNAME}/.bashrc
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
